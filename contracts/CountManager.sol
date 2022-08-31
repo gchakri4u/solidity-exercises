52fafbbdcbf2b520c3fba2fff0dbbe0b46732eb9
Ganesh <gchakri4u@gmail.com>
2022-08-31 13:25:43 -0500

    Adding events to Countmanager.sol

--- contracts/CountManager.sol
+++ contracts/CountManager.sol
@@ -1,17 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
-contract CountManager{
-    uint public count;
-    constructor(uint _count){
-        count = _count;
+
+contract CountManager {
+    uint256 public count;
+    bool public error = true;
+    event setEvent(uint256 count);
+
+    function getCount() public view returns (uint256) {
+        return count;
     }
-    function getCount() public view returns(uint){
-      return count;
+
+    function increment() public {
+        count = count + 1;
+        emit setEvent(count);
     }
-    function increment() public{
-      count = count +1;
-    }
-    function decrement() public{
-      count = count - 1;
+
+    function decrement() public {
+        count = count - 1;
     }
 }
